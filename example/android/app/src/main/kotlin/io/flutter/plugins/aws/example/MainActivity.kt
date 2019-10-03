17a7d6bb93e9f03c48a4835e742fadacc946d671
Andrew Chen <yongjhih@gmail.com>
2019-10-03 16:28:20 -0700

    Fix example

--- example/android/app/src/main/kotlin/io/flutter/plugins/aws/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/io/flutter/plugins/aws/example/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.flutter.plugins.aws.example
 
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -8,6 +9,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    FirebaseApp.initializeApp(this)
     GeneratedPluginRegistrant.registerWith(this)
   }
 }
